754a44d162aed07133ed403ea3f8c098afd53546
Renan Galego <107080173+RenanGalego@users.noreply.github.com>
2022-09-22 16:51:07 -0300

    atualização de bibliotecas
    importando novas bibliotecas e organizando o código

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #Arquivo principal
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+#Cria um reconhecedor
+def ouvir_mic():
+    mic = sr.Recognizer()
+    with sr.Microphone() as source:
+        mic.adjust_for_ambient_noise(source)
+        print("Estou ouvindo")
+        audio = mic.listen(source)
+
+    try:
+        frase = mic.recognize_google(audio, language='pt-BR')
+        print(f'Você disse: {frase}')
+    except sr.UnknownValueError:
+        print("Entendi não")
+        return frase
+
+ouvir_mic()
